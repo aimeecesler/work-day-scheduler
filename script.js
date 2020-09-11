554e5952c6d8574c229e6211dd3de6eebf54658f
ACE <67081507+aimeecesler@users.noreply.github.com>
2020-09-11 15:19:55 -0400

    added current date

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ $(document).ready(function () {
 var timeContainer = $("#time-container");
 var hours = [9,10,11,12,13,14,15,16,17];
 
+$("#currentDay").text(moment().format('dddd, MMMM Do, YYYY'));
+
 for (var i = 0; i < hours.length; i++){
 var timeRow = $("<div>").attr("class","row time-block");
 var hour = $("<div>").attr("class","hour col-sm-1");
@@ -14,4 +16,8 @@ timeContainer.append(timeRow);
 console.log(i);
 console.log(hours[i]);
 };
+
+
+
+
 });
