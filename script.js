f9a5e143074bce68a491c1ba9cba7cdc69b2cd44
ACE <67081507+aimeecesler@users.noreply.github.com>
2020-09-11 15:05:11 -0400

    added for loop for initial table setup with hours in an array

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+
+var timeContainer = $("#time-container");
+var hours = [9,10,11,12,13,14,15,16,17];
+
+for (var i = 0; i < hours.length; i++){
+var timeRow = $("<div>").attr("class","row time-block");
+var hour = $("<div>").attr("class","hour col-sm-1");
+hour.text(hours[i]);
+var textArea = $("<textarea>").attr("class","col-sm-10");
+var saveBtn = $("<button>").attr("class","saveBtn col-sm-1");
+timeRow.append(hour, textArea, saveBtn);
+timeContainer.append(timeRow);
+console.log(i);
+console.log(hours[i]);
+};
+
+});
