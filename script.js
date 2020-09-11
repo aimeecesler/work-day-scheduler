ac4858e78661cf174658fa0f3f07da63c1db9851
ACE <67081507+aimeecesler@users.noreply.github.com>
2020-09-11 16:40:40 -0400

    added if statements for color coding past present and future blocks

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
 $(document).ready(function () {
+  var timeContainer = $("#time-container");
+  var hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+  var currentHour = moment().format("H");
 
-var timeContainer = $("#time-container");
-var hours = [9,10,11,12,13,14,15,16,17];
+  $("#currentDay").text(moment().format("dddd, MMMM Do, YYYY"));
 
-$("#currentDay").text(moment().format('dddd, MMMM Do, YYYY'));
-
-for (var i = 0; i < hours.length; i++){
-var timeRow = $("<div>").attr("class","row time-block");
-var hour = $("<div>").attr("class","hour col-sm-1");
-hour.text(hours[i]);
-var textArea = $("<textarea>").attr("class","col-sm-10");
-var saveBtn = $("<button>").attr("class","saveBtn col-sm-1 fa fa-save");
-timeRow.append(hour, textArea, saveBtn);
-timeContainer.append(timeRow);
-console.log(i);
-console.log(hours[i]);
-};
-
-
-
-
+  for (var i = 0; i < hours.length; i++) {
+    var timeRow = $("<div>").addClass("row time-block");
+    var hour = $("<div>").addClass("hour col-sm-1");
+    hour.text(hours[i]);
+    console.log(moment().format("H"));
+    var textArea = $("<textarea>").addClass("col-sm-10");
+    if (currentHour == hours[i]) {
+      textArea.addClass("present");
+    } else if (currentHour > hours[i]) {
+      textArea.addClass("past");
+    } else {
+      textArea.addClass("future");
+    }
+    var saveBtn = $("<button>").attr("class", "saveBtn col-sm-1 fa fa-save");
+    timeRow.append(hour, textArea, saveBtn);
+    timeContainer.append(timeRow);
+  }
 });
